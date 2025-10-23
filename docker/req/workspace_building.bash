78400f9862b2901ddd96d4d944727aa9214b6d7a
dimadobriy <dobriy06@gmail.com>
2025-10-23 10:58:26 +0200

    Update workspace_building.bash

--- docker/req/workspace_building.bash
+++ docker/req/workspace_building.bash
@@ -4,9 +4,9 @@ EXEC_PATH=$PWD
 
 mkdir -p ../turtlebot3_ws/src && cd ../turtlebot3_ws/src
 
-git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3.git
-git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
-git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
+git clone -b noetic https://github.com/ROBOTIS-GIT/turtlebot3.git
+git clone -b noetic https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
+git clone -b noetic https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
 git clone -b noetic-devel https://github.com/ROBOTIS-GIT/DynamixelSDK.git
 git clone -b noetic-devel https://github.com/ROBOTIS-GIT/hls_lfcd_lds_driver.git
 git clone https://github.com/ROBOTIS-GIT/turtlebot3_machine_learning.git
